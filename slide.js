9ab3876a13fac37efaad5f9238b523971ee85b56
raj <raj@gmail.com>
2023-01-09 22:16:02 +0530

    spry store added

--- slide.js
+++ slide.js
@@ -1,4 +1,4 @@
-$('.owl-carousel').owlCarousel({
+$('.nisha').owlCarousel({
     loop: true,
     nav: false,
     dots: false,
@@ -8,11 +8,43 @@ $('.owl-carousel').owlCarousel({
     autoplaySpeed:5000,
     // stagePadding:50,
 });
-owl.on('mousewheel', '.owl-stage', function (e) {
-    if (e.deltaY > 0) {
-        owl.trigger('next.owl');
-    } else {
-        owl.trigger('prev.owl');
-    }
-    e.preventDefault();
-});
+
+
+
+    // // If you want to control the embeds, you’ll need to create a Player object.
+    // // You can pass either the `<div>` or the `<iframe>` created inside the div.
+    // const handstickPlayer = new Vimeo.Player('handstick');
+    // handstickPlayer.on('play', function() {
+    //     console.log('played the handstick video!');
+    // });
+
+    // const playerTwoPlayer = new Vimeo.Player('playertwo');
+    // playerTwoPlayer.on('play', function() {
+    //     console.log('played the player 2.0 video!');
+    // });
+
+    
+    $('.owl-carousel').owlCarousel({
+        loop: true,
+        nav: true,
+        dots: true,
+        items:4,
+        margin:20,
+        autoplay: true,
+        autoplayTimeout:7000,
+        autoplaySpeed:5000,
+        responsive:{
+            0:{
+                items:1,
+                dots:true
+            },
+            600:{
+                items:3,
+                dots:true,
+            },
+            1000:{
+                items:4,
+                dots:true,
+            }
+        }
+    });
